c6e4936ab4b2d505d0825c6d5fca832c8efc2ef9
Emil Leszczak <emil.leszczak@intramedium.pl>
2020-05-13 11:32:54 +0200

    Fix hash calculation for slashed data (#27)
    Co-authored-by: Paynow <kontakt@paynow.pl>

--- src/Paynow/Util/SignatureCalculator.php
+++ src/Paynow/Util/SignatureCalculator.php
@@ -25,7 +25,7 @@ class SignatureCalculator
         if (empty($data)) {
             throw new InvalidArgumentException('You did not provide any data');
         }
-        $this->hash = base64_encode(hash_hmac('sha256', json_encode($data, JSON_UNESCAPED_SLASHES), $signatureKey, true));
+        $this->hash = base64_encode(hash_hmac('sha256', json_encode($data), $signatureKey, true));
     }
 
     /**
