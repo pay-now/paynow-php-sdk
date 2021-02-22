399826111c5d6aee394e0c81825668d1c19b8117
Paynow <kontakt@paynow.pl>
2021-02-22 14:04:40 +0100

    Add retrieve payment methods support

---
+++ src/Paynow/Model/PaymentMethods/Type.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Paynow\Model\PaymentMethods;
+
+class Type
+{
+    public const BLIK = 'BLIK';
+    public const CARD = 'CARD';
+    public const PBL = 'PBL';
+}
---
+++ src/Paynow/Response/PaymentMethods/PaymentMethods.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Paynow\Response\PaymentMethods;
+
+use Paynow\Model\PaymentMethods\PaymentMethod;
+use Paynow\Model\PaymentMethods\Type;
+
+class PaymentMethods
+{
+    /**
+     * @var PaymentMethod[]
+     */
+    private $list;
+
+    public function __construct($body)
+    {
+        if (! empty($body)) {
+            foreach ($body as $group) {
+                if (! empty($group->paymentMethods)) {
+                    foreach ($group->paymentMethods as $item) {
+                        $this->list[] = new PaymentMethod(
+                            $item,
+                            $group->type,
+                            $item->name,
+                            $item->description,
+                            $item->image,
+                            $item->status
+                        );
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Retrieve all available payment methods
+     *
+     * @return PaymentMethod[]
+     */
+    public function getAll()
+    {
+        return $this->list;
+    }
+
+    /**
+     * Retrieve only Blik payment methods
+     *
+     * @return PaymentMethod[]
+     */
+    public function getOnlyBlik()
+    {
+        $blikPaymentMethods = [];
+        foreach ($this->list as $item) {
+            if (Type::BLIK === $item->getType()) {
+                $blikPaymentMethods[] = $item;
+            }
+        }
+
+        return $blikPaymentMethods;
+    }
+
+    /**
+     * Retrieve only Card payment methods
+     *
+     * @return PaymentMethod[]
+     */
+    public function getOnlyCards()
+    {
+        $cardPaymentMethods = [];
+        foreach ($this->list as $item) {
+            if (Type::CARD === $item->getType()) {
+                $cardPaymentMethods[] = $item;
+            }
+        }
+
+        return $cardPaymentMethods;
+    }
+
+    /**
+     * Retrieve only Pbl payment methods
+     *
+     * @return array
+     */
+    public function getOnlyPbls()
+    {
+        $pblPaymentMethods = [];
+        foreach ($this->list as $item) {
+            if (Type::PBL === $item->getType()) {
+                $pblPaymentMethods[] = $item;
+            }
+        }
+
+        return $pblPaymentMethods;
+    }
+}
