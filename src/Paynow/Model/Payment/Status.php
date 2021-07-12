bfd5936376c01bd785c18a584bfbf754e7160930
Paynow <kontakt@paynow.pl>
2021-07-12 15:25:53 +0200

    Add new statuses

--- src/Paynow/Model/Payment/Status.php
+++ src/Paynow/Model/Payment/Status.php
@@ -4,9 +4,12 @@ namespace Paynow\Model\Payment;
 
 class Status
 {
+    public const STATUS_CANCELLED = 'CANCELLED';
+    public const STATUS_CONFIRMED = 'CONFIRMED';
+    public const STATUS_ERROR = 'ERROR';
+    public const STATUS_EXPIRED = 'EXPIRED';
     public const STATUS_NEW = 'NEW';
     public const STATUS_PENDING = 'PENDING';
     public const STATUS_REJECTED = 'REJECTED';
-    public const STATUS_CONFIRMED = 'CONFIRMED';
-    public const STATUS_ERROR = 'ERROR';
+    public const STATUS_WAITING_FOR_CONFIRMATION = 'WAITING_FOR_CONFIRMATION';
 }
